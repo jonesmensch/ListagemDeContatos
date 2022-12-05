f77307af889d9586b1ed3b7b8b5cc1607ad37855
Jones Andrei Mensch <jones.mensch@outlook.com>
2022-12-05 15:20:02 -0300

    Add project files.

---
+++ ListagemDeContatos/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ListagemDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ListagemDeContatos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contatos()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Deletar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
